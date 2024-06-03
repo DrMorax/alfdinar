e967ec15688ff2067eb719f0794b1d8bf782fee9
Morax <abbasbusiness01@gmail.com>
2024-06-03 17:44:34 +0300

    fixed import of cart slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter-slice";
-import cartReducer from "./cart-slice";
 import userReducer from "./user-slice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    cart: cartReducer,
     user: userReducer,
   },
 });
