a200563f588c1f62fddf36adcb2cc5ed07380679
Morax <abbasbusiness01@gmail.com>
2024-06-07 01:23:27 +0300

    removed signout functionalities

--- src/app/profile/info.tsx
+++ src/app/profile/info.tsx
@@ -5,25 +5,25 @@ import { useRouter } from "next/navigation";
 export default function Info({ user }: { user: User | null }) {
   const router = useRouter();
 
-  const handleSignout = async () => {
-    const response = await fetch("/api/signout", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  // const handleSignout = async () => {
+  //   const response = await fetch("/api/signout", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
 
-    if (response.ok) {
-      router.push("/");
-    } else {
-      console.error("Sign-out failed");
-    }
-  };
+  //   if (response.ok) {
+  //     router.push("/");
+  //   } else {
+  //     console.error("Sign-out failed");
+  //   }
+  // };
 
   return (
     <div>
       <h1>Email: {user?.email}</h1>
-      <button onClick={handleSignout}>Sign Out</button>
+      {/* <button onClick={handleSignout}>Sign Out</button> */}
     </div>
   );
 }
--- src/pages/api/signout.ts
+++ src/pages/api/signout.ts
@@ -1,16 +1,16 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import createClient from "@/app/utils/supabase/api";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import createClient from "@/app/utils/supabase/api";
 
-export default async function handleSignout(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const supabase = createClient(req, res);
-  const { error } = await supabase.auth.signOut();
-  if (error) {
-    console.error(error);
-    return res.status(500).json({ error: error.message });
-  }
+// export default async function handleSignout(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const supabase = createClient(req, res);
+//   const { error } = await supabase.auth.signOut();
+//   if (error) {
+//     console.error(error);
+//     return res.status(500).json({ error: error.message });
+//   }
 
-  res.status(200).json({ success: true });
-}
+//   res.status(200).json({ success: true });
+// }
